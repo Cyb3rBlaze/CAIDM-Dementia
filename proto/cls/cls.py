2b18f4ef8929ae4413aa6034b3e685eba9ee5ca1
Brandhsu <brandondhsu@gmail.com>
2021-11-10 22:42:52 -0800

    model checkpointing

--- proto/cls/cls.py
+++ proto/cls/cls.py
@@ -7,44 +7,47 @@ from jarvis.utils.general import gpus, overload, tools as jtools
 
 import arch
 
+
 def prepare_client(paths, p):
 
-    client = Client(CLIENT_TRAINING if os.path.exists(CLIENT_TRAINING) else CLIENT_TEMPLATE, configs={
-        'batch': {
-            'size': p['batch_size'],
-            'fold': p['fold']}})
+    client = Client(
+        CLIENT_TRAINING if os.path.exists(CLIENT_TRAINING) else CLIENT_TEMPLATE,
+        configs={"batch": {"size": p["batch_size"], "fold": p["fold"]}},
+    )
 
     return client
+
 
 def load_existing(model, p):
 
     # --- Create output_dir
-    os.makedirs(p['output_dir'], exist_ok=True)
+    os.makedirs(p["output_dir"], exist_ok=True)
 
     # --- Load existing model if present
     if os.path.exists(MODEL_NAME):
-        print('Loading existing model weights: {}'.format(MODEL_NAME))
+        print("Loading existing model weights: {}".format(MODEL_NAME))
         model.load_weights(MODEL_NAME)
 
     return model
 
+
 # =======================================================
-# PREPARATION 
+# PREPARATION
 # =======================================================
 
 # --- Autoselect GPU
 gpus.autoselect()
 
 # --- Look up path
-paths = jtools.get_paths('adni')
+paths = jtools.get_paths("adni")
 
 # --- Prepare hyperparams
-p = params.load('./hyper.csv', row=0)
+p = params.load("./hyper.csv", row=0)
 
 # --- Set constants
-CLIENT_TEMPLATE = '/home/mmorelan/proj/dementia/yml/client-3d-raw-mod.yml'
-CLIENT_TRAINING = '{}/client.yml'.format(p['output_dir'])
-MODEL_NAME = '{}/model.hdf5'.format(p['output_dir'])
+CLIENT_TEMPLATE = "/home/mmorelan/proj/dementia/yml/client-3d-raw-mod.yml"
+CLIENT_TRAINING = "{}/client.yml".format(p["output_dir"])
+MODEL_NAME = "{}/model.hdf5".format(p["output_dir"])
 
 # --- Prepare client
 client = prepare_client(paths, p)
@@ -56,7 +59,7 @@ model = arch.cls_v01(client.get_inputs(Input))
 model = load_existing(model, p)
 
 # =======================================================
-# TRAINING LOOP 
+# TRAINING LOOP
 # =======================================================
 
 # --- Assume a 400:100 ratio of train:valid
@@ -64,26 +67,35 @@ steps_per_epoch = 100
 validation_freq = 1
 
 # --- Determine total loop iterations needed
-epochs = int(p['iterations'] / steps_per_epoch)
+epochs = int(p["iterations"] / steps_per_epoch)
 
-# --- Prepare Tensorboard 
-log_dir = '{}/jmodels/logdirs/{}'.format(
-    os.path.dirname(p['output_dir']),
-    os.path.basename(p['output_dir']))
+# --- Prepare Tensorboard
+log_dir = "{}/jmodels/logdirs/{}".format(
+    os.path.dirname(p["output_dir"]), os.path.basename(p["output_dir"])
+)
 
-tensorboard_callback = callbacks.TensorBoard(log_dir=log_dir, profile_batch=0)
-early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-lr_decay = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=1)
+# --- Prepare Model checkpoint
+ckpt_dir = "{}/jmodels/checkpoints/{}".format(
+    os.path.dirname(p["output_dir"]), os.path.basename(p["output_dir"])
+)
 
-# --- Train
+# --- Define training callbacks
+tensorboard_log = callbacks.TensorBoard(log_dir=log_dir, profile_batch=0)
+model_checkpoint = callbacks.ModelCheckpoint(log_dir=ckpt_dir)
+early_stopping = callbacks.EarlyStopping(
+    monitor="val_loss", patience=5, restore_best_weights=True
+)
+lr_decay = callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=5)
+
+# --- Train the model
 model.fit(
-    x=gen_train, 
+    x=gen_train,
     epochs=epochs,
-    steps_per_epoch=steps_per_epoch, 
+    steps_per_epoch=steps_per_epoch,
     validation_data=gen_valid,
     validation_steps=steps_per_epoch,
     validation_freq=validation_freq,
-    callbacks=[tensorboard_callback]
+    callbacks=[tensorboard_log, model_checkpoint],
 )
 
 # --- Save model
--- proto/ctr/ctr.py
+++ proto/ctr/ctr.py
@@ -7,37 +7,41 @@ from jarvis.train.client import Client
 from jarvis.utils.general import gpus, overload
 
 # --- Define path of clients
-AD_CLIENT_PATH = '/home/mmorelan/proj/dementia/template/contrastive/ctr_v3/client-3d-raw_AD_AV45_only.yml'
-CN_CLIENT_PATH = '/home/mmorelan/proj/dementia/template/contrastive/ctr_v3/client-3d-raw_CN_AV45_only.yml'
+AD_CLIENT_PATH = "/home/mmorelan/proj/dementia/template/contrastive/ctr_v3/client-3d-raw_AD_AV45_only.yml"
+CN_CLIENT_PATH = "/home/mmorelan/proj/dementia/template/contrastive/ctr_v3/client-3d-raw_CN_AV45_only.yml"
 INPUT_SHAPE = (96, 160, 160, 1)
+
 
 @overload(Client)
 def preprocess(self, arrays, **kwargs):
-    
+
     # --- Extract pre-calculated whole exam mu/sd and normalize
-    arrays['xs']['dat'] = (arrays['xs']['dat'] - kwargs['row']['mu']) / kwargs['row']['sd']
-    
+    arrays["xs"]["dat"] = (arrays["xs"]["dat"] - kwargs["row"]["mu"]) / kwargs["row"][
+        "sd"
+    ]
+
     # --- Scale to 0/1 using 5/95 percentiles
-    lower = np.percentile(arrays['xs']['dat'], 1)
-    upper = np.percentile(arrays['xs']['dat'], 99)
-    arrays['xs']['dat'] = arrays['xs']['dat'].clip(min=lower, max=upper)
-    arrays['xs']['dat'] = (arrays['xs']['dat'] - lower) / (upper - lower)
-    
+    lower = np.percentile(arrays["xs"]["dat"], 1)
+    upper = np.percentile(arrays["xs"]["dat"], 99)
+    arrays["xs"]["dat"] = arrays["xs"]["dat"].clip(min=lower, max=upper)
+    arrays["xs"]["dat"] = (arrays["xs"]["dat"] - lower) / (upper - lower)
+
     return arrays
+
 
 # --- Autoselect GPU
 gpus.autoselect()
 
 # --- Prepare hyperparams
-p = params.load('./hyper.csv', row=0)
+p = params.load("./hyper.csv", row=0)
 
-MODEL_NAME = '{}/model.hdf5'.format(p['output_dir'])
+MODEL_NAME = "{}/model.hdf5".format(p["output_dir"])
 
 # --- Prepare model
 inputs = {
-    'pos': Input(shape=INPUT_SHAPE, name='pos'),
-    'unk': Input(shape=INPUT_SHAPE, name='unk'),
-    'neg': Input(shape=INPUT_SHAPE, name='neg'),
+    "pos": Input(shape=INPUT_SHAPE, name="pos"),
+    "unk": Input(shape=INPUT_SHAPE, name="unk"),
+    "neg": Input(shape=INPUT_SHAPE, name="neg"),
 }
 
 gen_train = contrastive_generator()
@@ -50,18 +54,25 @@ steps_per_epoch = 100
 validation_freq = 1
 
 # --- Determine total loop iterations needed
-epochs = int(p['iterations'] / steps_per_epoch)
+epochs = int(p["iterations"] / steps_per_epoch)
 
-# --- Prepare Tensorboard 
-log_dir = '{}/jmodels/logdirs/{}'.format(
-    os.path.dirname(p['output_dir']),
-    os.path.basename(p['output_dir']))
+# --- Prepare Tensorboard
+log_dir = "{}/jmodels/logdirs/{}".format(
+    os.path.dirname(p["output_dir"]), os.path.basename(p["output_dir"])
+)
 
+# --- Prepare Model checkpoint
+ckpt_dir = "{}/jmodels/checkpoints/{}".format(
+    os.path.dirname(p["output_dir"]), os.path.basename(p["output_dir"])
+)
 
 # --- Define training callbacks
 tensorboard_log = callbacks.TensorBoard(log_dir=log_dir, profile_batch=0)
-early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-lr_decay = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5)
+model_checkpoint = callbacks.ModelCheckpoint(log_dir=ckpt_dir)
+early_stopping = callbacks.EarlyStopping(
+    monitor="val_loss", patience=5, restore_best_weights=True
+)
+lr_decay = callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=5)
 
 # --- Train the model
 model.fit(
@@ -71,7 +82,7 @@ model.fit(
     validation_data=gen_valid,
     validation_steps=steps_per_epoch,
     validation_freq=validation_freq,
-    callbacks=[tensorboard_log]
+    callbacks=[tensorboard_log, model_checkpoint],
 )
 
 # --- Save model
